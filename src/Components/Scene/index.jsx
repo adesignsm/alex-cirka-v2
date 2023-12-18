0742ec540e394a397c644ceb9ea68d8fd8ff7347
Akash Mulye <akashmulye97@gmail.com>
2023-12-18 11:40:23 -0500

    removed defaults

--- src/Components/Scene/index.jsx
+++ src/Components/Scene/index.jsx
@@ -30,14 +30,42 @@ const Scene = () => {
     });
   }, []);
 
-  const PlaneWithTexture = ({ source, xPos, yPos, zPos }) => {
+  useEffect(() => {
+    sanityClient.fetch('*[_type == "videoReel"]').then((result) => {
+      const assetRef = result[0].video.asset._ref;
+      const withoutFilePrefix = assetRef.replace('file-', '');
+      const modifiedAssetRef = withoutFilePrefix.replace(/-(?=[^-]*$)/, '.');
+
+      console.log('https://cdn.sanity.io/files/bpugi0wz/production/' + modifiedAssetRef);
+    }).catch((error) => {
+      // eslint-disable-next-line
+      console.error(error);
+    });
+  }, []);
+
+  const PlaneWithTexture = ({ source, name, xPos, yPos, zPos }) => {
     const planeRef = useRef();
     const texture = useTexture(source);
+    const hoverOffset = useRef(0);
+
+    useFrame(() => {
+      planeRef.current.position.y = yPos + Math.sin(hoverOffset.current) * 0.05;
+      hoverOffset.current += 0.01;
+    });
+
+    const regex = /(\d+)x(\d+)\.png$/;
+    const match = source.match(regex);
+    let xWidth, yHeight;
+
+    if (match) {
+      xWidth = parseInt(match[0]);
+      yHeight = parseInt(match[1]);
+    }
   
     return (
       <>
-        <mesh ref={planeRef} scale={0.1} position={[xPos, yPos, zPos]}>
-          <planeGeometry args={[15, 10]} ref={geometry => geometry && geometry.computeVertexNormals()} />
+        <mesh name={name} ref={planeRef} scale={0.1} position={[xPos, yPos, zPos]}>
+          <planeGeometry args={[xWidth / 100, yHeight / 100]} ref={geometry => geometry && geometry.computeVertexNormals()} />
           <meshBasicMaterial map={texture} side={DoubleSide} transparent={true} depthTest={false} depthWrite={false}/>
         </mesh>
       </>
@@ -48,17 +76,18 @@ const Scene = () => {
     <>
       <div id="home-canvas">
         <Suspense fallback={null}>
-          <Canvas frameloop="always" camera={{ fov: 50, near: 1, far: 100000, position: [0, 0, 10] }}>
+          <Canvas frameloop="always" camera={{ fov: 50, near: 1, far: 100000, position: [0, 0, 15] }}>
             <ambientLight intensity={1} />
-            <OrbitControls enableZoom={true} />
+            <OrbitControls enableZoom={false} />
             {data && (
                 data.map((thumbnail, index) => {
                   return (
                     <PlaneWithTexture 
-                      key={index} 
+                      key={index}
+                      name={`mesh-${index}`}
                       source={thumbnail} 
-                      xPos={(Math.random() - 0.5) * 10}
-                      yPos={(Math.random() - 0.5) * 3}
+                      xPos={(Math.random() - 0.5) * 15}
+                      yPos={(Math.random() - 0.5) * 5}
                       zPos={(Math.random() - 0.5) * 10}
                     />
                   )
@@ -68,7 +97,7 @@ const Scene = () => {
               {/* <DepthOfField focusDistance={15} bokehScale={2.5} focusRange={20}/> */}
               {/* <DotScreen scale={10} angle={Math.PI * 0.5}/> */}
               {/* <Scanline density={2} /> */}
-              <Bloom luminanceThreshold={0.5} luminanceSmoothing={0.005} height={window.innerHeight} />
+              <Bloom luminanceThreshold={0.5} luminanceSmoothing={0.005} height={window.innerHeight} width={window.innerWidth}/>
               {/* <Noise opacity={0.1} /> */}
             </EffectComposer>
           </Canvas>
